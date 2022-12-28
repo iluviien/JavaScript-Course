e0506d2c559cc473f9c13ce7247535ea8757bbcb
iluviien <stefanica695@gmail.com>
2022-12-28 14:46:17 +0000

    Save changes to JS course

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,78 @@
+// Assignment 1
+/* LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+
+// let country = "Portugal";
+//let continent = "Europe";
+let population = 10;
+//console.log(country);
+//console.log(continent);
+//console.log(population);
+
+// Assignment 2
+/* LECTURE: Data Types
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+*/
+
+/*let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+*/
+//Assignment 3
+/*LECTURE: let, const and var
+1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens
+*/
+
+language = "portuguese";
+const country = "Portugal";
+const continent = "Europe";
+const isIsland = false;
+//isIsland = true;
+
+//Assignment 4
+/* LECTURE: Basic Operators
+1. If your country split in half, and each half would contain half the population,
+then how many people would live in each half?
+2. Increase the population of your country by 1 and log the result to the console
+3. Finland has a population of 6 million. Does your country have more people than
+Finland?
+4. The average population of a country is 33 million people. Does your country
+have less people than the average country?
+5. Based on the variables you created, create a new variable 'description'
+which contains a string with this format: 'Portugal is in Europe, and its 11 million
+people speak portuguese.
+*/
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+const description =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " million people speak " +
+  language;
+console.log(description);
+
+//Assignment 5
+/* LECTURE: Strings and Template Literals
+1. Recreate the 'description' variable from the last assignment, this time
+using the template literal syntax.
+*/
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,73 @@
+/* Unit 1
+let js = "amazing";
+
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+let PI = 3.1415;
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof "Jonas");
+//console.log(typeof 23);
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+
+console.log(typeof year);
+
+console.log(typeof null);
+
+let age = 30;
+age = 31;
+const birthYear = 1991;
+//birthYear = 1990;
+//const job;
+var job = "programmer";
+job = "teacher";
+
+lastName = "Schedtmann";
+console.log(lastName);
+
+const now = 2037;
+const ageAlina = now - 1994;
+const ageSarah = now - 2018;
+console.log(ageAlina, ageSarah);
+
+console.log(ageAlina * 2, ageAlina / 2, 2 ** 3);
+// 2 ** 3 means to the power of 3 = 2 * 2 * 2;
+
+const firstName = "Alina";
+const lastName = "Stefanica";
+
+console.log(firstName + " " + lastName);
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100 ;
+x++; // x = x + 1;
+x--; // x = x - 1 ;
+console.log(x);
+
+// Comparison operators
+console.log(ageAlina > ageSarah); // >, <. >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 19;
+console.log(now - 1991 > now - 2018);
+*/
