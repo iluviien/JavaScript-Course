b208d4f0f41a04740d1234560205a533e020f4fd
iluviien <stefanica695@gmail.com>
2023-01-11 14:12:55 +0000

    Add progress

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -10,29 +10,29 @@
 
 // Functions declarations vs expressions
 
-function percentageOfWorld1(population) {
-  return (population / 7900) * 100;
-}
-const percentageOfWorld2 = function (population) {
-  return (population / 7900) * 100;
-};
-const percPortugal = percentageOfWorld1(10);
-const percChina1 = percentageOfWorld1(1441);
-const percUSA1 = percentageOfWorld1(332);
+// function percentageOfWorld1(population) {
+//   return (population / 7900) * 100;
+// }
+// const percentageOfWorld2 = function (population) {
+//   return (population / 7900) * 100;
+// };
+// const percPortugal = percentageOfWorld1(10);
+// const percChina1 = percentageOfWorld1(1441);
+// const percUSA1 = percentageOfWorld1(332);
 
-// console.log(percPortugal, percChina1, percUSA1);
+// // console.log(percPortugal, percChina1, percUSA1);
 
-// Arrow functions assignment
-const percentageOfWorld3 = population => (population / 7900) * 100;
-const percPortugal3 = percentageOfWorld3(10);
-const percChina3 = percentageOfWorld3(1441);
-const percUSA33 = percentageOfWorld3(332);
+// // Arrow functions assignment
+// const percentageOfWorld3 = population => (population / 7900) * 100;
+// const percPortugal3 = percentageOfWorld3(10);
+// const percChina3 = percentageOfWorld3(1441);
+// const percUSA33 = percentageOfWorld3(332);
 
-const describePopulation = function (country, population) {
-  const percentage = percentageOfWorld1(population);
-  const description = `${country} has ${population} million people, which is about ${percentage} `;
-  console.log(description);
-};
-describePopulation("Portugal", 10);
-describePopulation("China", 1441);
-describePopulation("USA ", 331);
+// const describePopulation = function (country, population) {
+//   const percentage = percentageOfWorld1(population);
+//   const description = `${country} has ${population} million people, which is about ${percentage} `;
+//   console.log(description);
+// };
+// describePopulation("Portugal", 10);
+// describePopulation("China", 1441);
+// describePopulation("USA ", 331);
--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -58,3 +58,77 @@
 //   return juice;
 // }
 // console.log(fruitProcessor(2, 3));
+
+//
+
+//CODING CHALLENGE 1
+
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+// console.log(calcAverage(3, 4, 5));
+
+// let scoreDolphins = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
+
+// console.log(scoreDolphins, scoreKoalas);
+
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins with the trophy(${avgDolphins} vs ${avgKoalas})`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas win the trophy.(${avgKoalas} vs ${avgDolphins})`);
+//   } else {
+//     console.log(`No team wins...`);
+//   }
+// };
+
+// checkWinner(scoreDolphins, scoreKoalas);
+// checkWinner(576, 111);
+
+// //Test 2 data
+
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+
+// checkWinner(scoreDolphins, scoreKoalas);
+
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const years0 = new Array(1991, 1984, 2008, 2020); // creating a new array using the array function
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+
+console.log(friends);
+
+const alina = ["Alina", "Stefanica", 2037 - 1994, "student", friends];
+console.log(alina);
+
+// Excercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+const years = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1])
+];
+
+console.log(ages);
