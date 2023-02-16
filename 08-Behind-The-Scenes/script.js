3e16d7f102222d0a4d04ac981a994bcb52c580e2
iluviien <stefanica695@gmail.com>
2023-02-16 21:48:28 +0000

    ADD THE PROGRESS

--- 08-Behind-The-Scenes/script.js
+++ 08-Behind-The-Scenes/script.js
@@ -23,6 +23,8 @@
 // const firstName = "Alina";
 // calcAge(1991);
 
+/*
+
 // Variables
 console.log(me);
 // console.log(job);
@@ -60,3 +62,96 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+*/
+//console.log(this);
+
+/* const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  //console.log(this);
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  }
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017
+};
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
+*/
+
+/*
+const jonas = {
+  firstName: "Jonas",
+  year: 1991,
+  calcAge: function () {
+    //console.log(this);
+    console.log(2037 - this.year);
+
+    //Solution 1
+    // const self = this; //self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+
+    // console.log(this.year >= 1981 && self.year <= 1996);
+    //    };
+
+    //Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+
+    isMillenial();
+  },
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  }
+};
+jonas.greet();
+jonas.calcAge();
+
+//arguments keyword
+
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
+*/
+
+let age = 30;
+let oldAge = age;
+age = 31;
+
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: "Jonas",
+  age: 30
+};
+const friend = me;
+
+friend.age = 27;
+console.log("Friend:", friend);
+console.log("Me", me);
