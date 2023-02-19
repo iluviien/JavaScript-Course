d6ce8c753814247d017ab35f48b625731b20f485
iluviien <stefanica695@gmail.com>
2023-02-19 20:01:42 +0000

    Adding progress to the course

--- 08-Behind-The-Scenes/script.js
+++ 08-Behind-The-Scenes/script.js
@@ -139,6 +139,7 @@ var addArrow = (a, b) => {
 addArrow(2, 5, 8);
 */
 
+/*
 let age = 30;
 let oldAge = age;
 age = 31;
@@ -155,3 +156,39 @@ const friend = me;
 friend.age = 27;
 console.log("Friend:", friend);
 console.log("Me", me);
+*/
+
+// Primitive types
+
+let lastName = "Williams";
+let oldLastName = lastName;
+lastName = "Davis";
+console.log(lastName, oldLastName);
+
+// reference types
+const jessica = {
+  firstName: "Jessica",
+  lastName: "Williams",
+  age: 27
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = "Davis";
+console.log("Before marriage:", jessica);
+console.log("After marriage:", marriedJessica);
+// marriedJessica = {}
+
+//copying objects
+const jessica2 = {
+  firstName: "Jessica",
+  lastName: "Williams",
+  age: 27,
+  family: ["Alica", "Bob"]
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = "Davis";
+
+jessicaCopy.family.push("Mary");
+jessicaCopy.family.push("John");
+console.log("Before marriage:", jessica2);
+console.log("After marriage: ", jessicaCopy);
