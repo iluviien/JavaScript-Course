24c3751a68471af3f1d830ce53f7e9b9b6eb4892
iluviien <stefanica695@gmail.com>
2023-02-20 15:19:47 +0000

    Adding progress, unit 104 x q cd..
    d
    
    c
    d
    c
    ds
    sdcsd

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -7,11 +7,81 @@ const restaurant = {
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
 
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22
+    },
+    fri: {
+      open: 11,
+      close: 23
+    },
+    sat: {
+      open: 0, //Open 24 hrs
+      close: 24
+    }
+  },
+
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} will be delivered to ${address} at ${time}`
+    );
   }
 };
 
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "Via del Sole, 21",
+  mainIndex: 2,
+  starterIndex: 2
+});
+
+restaurant.orderDelivery({
+  address: "Via del Sole, 21",
+  starterIndex: 1
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//Default values
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//Mutating variables
+
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+// nested objects
+
+const {
+  fri: { open: o, close: c }
+} = openingHours;
+console.log(o, c);
+
+/*
+// Destructuring arrays
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -50,3 +120,4 @@ console.log(i, j, k);
 // default values
 const [p = 1, q = 1, r = 1] = [8];
 console.log(p, q, r);
+*/
