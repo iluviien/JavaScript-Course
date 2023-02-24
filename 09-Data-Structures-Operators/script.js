f8651fb263e9de6319bf30c3f9057db3a78689de
iluviien <stefanica695@gmail.com>
2023-02-24 21:44:36 +0000

    God give me strength

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -47,7 +47,16 @@ const restaurant = {
     console.log(otherIngredients);
   }
 };
-
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const item of menu) console.log(item);
+// old school way according to mr. Jonas
+// for (const item of menu.entries()) {
+//   console.log(`${item[0] + 1}: ${item[1]}`);
+// }
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+/*
 //Local assignment operator
 const rest1 = {
   name: "Capri",
@@ -76,6 +85,8 @@ rest2.owner &&= "<ANONYMOUS>";
 console.log(rest1);
 
 console.log(rest2);
+
+*/
 
 ///////////////////////
 /*
@@ -289,4 +300,79 @@ console.log(i, j, k);
 // default values
 const [p = 1, q = 1, r = 1] = [8];
 console.log(p, q, r);
+*/
+//////////////////////
+// CHALLENGE 1
+
+/*const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussian Dortmund",
+  players: [
+    [
+      "Neure",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski"
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze"
+    ]
+  ],
+  score: "4.0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5
+  }
+};
+//1.Create one player array for each team (variables 'players1' and 'players2')
+const [players1, players2] = game.players;
+console.log(players1, players2);
+//2.The first player in any player array is the goalkeeper and the others are field players.
+//For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+//3.Create an array 'allPlayers' containing all players of both teams (22 players)
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+//4.During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+const players1Final = [...players1, "Thiago", "Coutinho", "Periscic"];
+//5.Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+const {
+  odds: { team1, x: draw, team2 }
+} = game;
+console.log(team1, draw, team2);
+
+//6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+// TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored`);
+};
+printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+printGoals("Davies", "Muller");
+printGoals(...game.scored);
+//7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+team1 < team2 && console.log("Team 1 is more likely to win");
+team1 > team2 && console.log("Team 2 is more likely to win");
 */
