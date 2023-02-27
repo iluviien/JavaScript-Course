303a6906b3e7277a162bae8c13088f8f9a6f4271
iluviien <stefanica695@gmail.com>
2023-02-27 16:34:40 +0000

    I have no idea what I am doing

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -45,6 +45,111 @@ const restaurant = {
   }
 };
 
+// String methods practice
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+const getCode = str => str.slice(0, 3).toUpperCase();
+for (const flight of flights.split("+")) {
+  const [type, from, to, time] = flight.split(";");
+  const output = `${type.startsWith("_Delayed") ? "😡" : " "}${type.replaceAll(
+    "_",
+    "  "
+  )} ${getCode(from)} ${getCode(to)} (${time.replace(":", "h")})`.padStart(36);
+  console.log(output);
+}
+//////////////////////////////////////////////////////////////////////////
+
+/*
+//Challenge
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+Afterwards, test with your own test data!
+GOOD LUCK 😀
+*/
+/*
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+document.querySelector("button").addEventListener("click", function () {
+  const text = document.querySelector("textarea").value;
+  const rows = text.split("\n");
+  console.log(rows);
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split("_");
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+  }
+});
+*/
+////////////////////////////////////////////
+// Split and join
+
+/*console.log("a+very+nice+string".split("+"));
+console.log("Alina Stefanica".split(" "));
+const [firstName, lastName] = "Alina Stefanica".split(" ");
+const newName = ["Mrs.", firstName, lastName.toUpperCase()].join(" ");
+console.log(newName);
+const capitaliseName = function (name) {
+  const names = name.split(" ");
+  const namesUpper = [];
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1)); -- first approace
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase())); //second approach
+  }
+  console.log(namesUpper.join(" "));
+};
+capitaliseName("jessica ann smith davis");
+capitaliseName("jonas schmedtmann");
+
+//Padding a string
+const message = "Go to gate 23!";
+console.log(message.padStart(25, "+").padEnd(30, "+"));
+console.log("Alina".padStart(25, "+").padEnd(30, "+"));
+
+const maskCreditCard = function (number) {
+  const str = number + "";
+  const last = str.slice(-4);
+  return last.padStart(str.length, "*");
+};
+
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard("234563234242352352"));
+
+//Repeat method
+const message2 = "Bad weather...All departures delayed... ";
+console.log(message2.repeat(5));
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line`.repeat(n));
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+*/
+
+/*
+///////Working with strings part 2
 const airline = "Tap Air Portugal";
 ///////////////////////////////Working with strings part 2
 console.log(airline.toLowerCase());
@@ -98,7 +203,7 @@ const checkBaggage = function (items) {
 checkBaggage(" I have a laptop, some Food and a pocket knife");
 checkBaggage("Socks and camera");
 checkBaggage("Got some snacks and a gun for protection");
-
+*/
 /////////////////////////////////Working with strings Part 1
 /*
 const plane = "A320";
